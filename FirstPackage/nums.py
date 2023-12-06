23381d04d2067ae565052b5b3a830dcfe766bf2b
Yuriy Halenko <yhalenko@gmail.com>
2023-12-06 20:58:51 +0000

    Module strings has been added; new function mul.

--- FirstPackage/nums.py
+++ FirstPackage/nums.py
@@ -1,11 +1,16 @@
 def plus(a, b):
     return (a + b)
 
-text = "new text"
 
-def minus(a,b):
-    return (a-b)
+def minus(a, b):
+    return (a - b)
+
+
+def mul(a, b):
+    return a * b
+
 
 if __name__ == '__main__':
-    print(plus(2,3))
-    print(minus(3,3))
+    print(plus(2, 3))
+    print(minus(3, 3))
+    print(mul(3, 5))
